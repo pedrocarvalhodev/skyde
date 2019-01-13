56a8dd9760da0a9ba0e089a9829d061e11eff3c7
pedro <pedro9@github.com>
2019-01-12 22:10:31 -0200

    added seconds working plotly dash

--- dash_app.py
+++ dash_app.py
@@ -7,6 +7,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import pandas as pd
+import plotly.graph_objs as go
+
 server = flask.Flask(__name__)
 
 
@@ -36,8 +39,47 @@ dash_app1.layout = html.Div(children=[
 ])
 #----------------------
 
-dash_app2 = dash.Dash(__name__, server = server, url_base_pathname='/reports/')
-dash_app2.layout = html.Div([html.H1('Hi there, I am app2 for reports')])
+#dash_app2 = dash.Dash(__name__, server = server, url_base_pathname='/reports/')
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+dash_app2 = dash.Dash(__name__, server = server, url_base_pathname='/reports/', external_stylesheets=external_stylesheets)
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
+
+
+dash_app2.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
+
+#dash_app2.layout = html.Div([html.H1('Hi there, I am app2 for reports')])
 #dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
 
 @server.route('/')
