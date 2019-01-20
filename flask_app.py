fce81e3536278cfccbadaa18345fd9d5cbd60789
pedro <pedro9@github.com>
2019-01-20 20:36:01 -0200

    working flask, not gunicorn

--- flask_app.py
+++ flask_app.py
@@ -2,11 +2,21 @@ import os
 import io
 import base64
 import time
-import flask
 import dill as pickle
 import numpy as np
 import pandas as pd
 import models.gridCV.model_pipeline as model_gridCV
+
+
+import flask
+#import werkzeug
+from werkzeug.wsgi import DispatcherMiddleware
+from werkzeug.serving import run_simple
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
 
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from matplotlib import pyplot as plt
@@ -321,5 +331,206 @@ def get_timeseries():
 		return '<center><img src="data:image/png;base64,{}"></center>'.format(plot_url)
 
 
+###################################
+###################################
+
+#----------------------
+# for deployment, pass app.server (which is the actual flask app) to WSGI etc
+dash_app1 = dash.Dash(__name__, server = app, url_base_pathname='/dashboard/' )
+
+dash_app1.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+#----------------------
+
+#dash_app2 = dash.Dash(__name__, server = server, url_base_pathname='/reports/')
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+dash_app2 = dash.Dash(__name__, server = app, url_base_pathname='/reports/', external_stylesheets=external_stylesheets)
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
+
+
+dash_app2.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
+
+#---------------------------------------------
+external_stylesheets_3 = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+#dash_app3 = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+dash_app3 = dash.Dash(__name__, server = app, url_base_pathname='/reports_scatter/', external_stylesheets=external_stylesheets_3)
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/'
+    'cb5392c35661370d95f300086accea51/raw/'
+    '8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/'
+    'indicators.csv')
+
+available_indicators = df['Indicator Name'].unique()
+
+dash_app3.layout = html.Div([
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                id='xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Fertility rate, total (births per woman)'
+            ),
+            dcc.RadioItems(
+                id='xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+        style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Life expectancy at birth, total (years)'
+            ),
+            dcc.RadioItems(
+                id='yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic'),
+
+    dcc.Slider(
+        id='year--slider',
+        min=df['Year'].min(),
+        max=df['Year'].max(),
+        value=df['Year'].max(),
+        marks={str(year): str(year) for year in df['Year'].unique()}
+    )
+])
+
+@dash_app3.callback(
+    dash.dependencies.Output('indicator-graphic', 'figure'),
+    [dash.dependencies.Input('xaxis-column', 'value'),
+     dash.dependencies.Input('yaxis-column', 'value'),
+     dash.dependencies.Input('xaxis-type', 'value'),
+     dash.dependencies.Input('yaxis-type', 'value'),
+     dash.dependencies.Input('year--slider', 'value')])
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type,
+                 year_value):
+    dff = df[df['Year'] == year_value]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+            text=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear' if xaxis_type == 'Linear' else 'log'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear' if yaxis_type == 'Linear' else 'log'
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+#-------------------------------------------------------------
+
+#dash_app2.layout = html.Div([html.H1('Hi there, I am app2 for reports')])
+#dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
+
+#@app.route('/timeseries/')
+#def timeseries():
+#    return flask.render_template('timeseries.html')
+
+
+@app.route('/hello/')
+def hello():
+    return 'hello world!'
+
+@app.route('/dashboard/')
+def render_dashboard():
+    return flask.redirect('/dash1')
+
+
+@app.route('/reports/')
+def render_reports():
+    return flask.redirect('/dash2')
+
+@app.route('/reports_scatter/')
+def render_reports_scatter():
+    return flask.redirect('/dash3')
+
+
+application = DispatcherMiddleware(app, {
+    '/dash1': dash_app1.server,
+    '/dash2': dash_app2.server,
+    '/dash3': dash_app3.server,
+})
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
+	#application.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
+	run_simple('0.0.0.0', 8080, application, use_reloader=True, use_debugger=True)
+
